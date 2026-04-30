3bea97068e9c634b7c818c8659d7445410cfffd2
Tu Nombre <frantomasino@hotmail.com>
2026-04-30 10:38:57 -0300

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,8 +73,11 @@ export const metadata: Metadata = {
       "Vehículos verificados, atención premium y los mejores precios del sur del Gran Buenos Aires.",
     images: ["/banner-2.jpg"],
   },
-  manifest: "/site.webmanifest",
+ manifest: "/site.webmanifest",
   category: "automotive",
+  verification: {
+    google: "UsX8QnChnimUNbESC4SczMi_WvsZlgDMQ8S77rwaguQ",
+  },
 };
 
 const localBusinessSchema = {
